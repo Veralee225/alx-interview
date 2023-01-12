f70931843950db4677c2cf843c87901dfeb9fa32
Veralee225 <Veralee225@gmail.com>
2023-01-12 03:30:07 +0100

    fix: rewrote

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3
-'''The minimum operations coding challenge
-'''
+
+"""
+   In a text file, there is a single character H. Your text editor
+   can execute only two operations in this file: Copy All and Paste.
+   Given a number n, write a method that calculates the fewest
+   number of operations needed to result in
+   exactly n H characters in the file.
+"""
 
 
 def minOperations(n):
-    '''Computes the fewest number of operations needed
-    to result in exactly n H characters.
-    '''
-    if not isinstance(n, int):
-        return 0
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    # print('H', end='')
-    while done < n:
-        if clipboard == 0:
-            #int (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count +=2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n- done) % done == 0:
-            # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-        # print('')
-        return ops_count
+    num_ops = 0
+    min_ops = 2
+    while n > 1:
+        while n % min_ops == 0:
+            num_ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return num_ops
