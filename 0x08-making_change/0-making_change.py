f714edfeffe9a265c89e5b1039b60029a83cd551
Veralee225 <Veralee225@gmail.com>
2023-02-23 15:18:00 +0100

    feat: task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Make change
+"""
+
+def makeChange(coins, total):
+    """
+    makeChange-function to find fewest number of coins needed to meet total
+    Arguments:
+        coins: the given coins
+        total: the given total we need to acheive
+    Returns:
+        the fewest number of coins to meet total
+    """
+
+    if total <= 0:
+        return 0
+    sorted_coins = sorted(coins, reverse=True)
+    coins_nbr = {}
+    for cn in sorted_coins:
+        if total % cn == 0:
+            coins_nbr[cn] = total / cn
+            rslt = sum(coins_nbr.values())
+            return int(rslt)
+        else:
+            coins_nbr[cn] = total // cn
+            total = total % cn
+    return -1
